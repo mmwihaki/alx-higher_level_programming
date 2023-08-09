68b9a4f83ce5cb71dcb0dcb7c99ede3fe1b6bd54
mmwihaki <marymwihaki16@gmail.com>
2023-08-09 21:46:35 +0300

    fizz buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for num in range(1, 100):
+        if num %  3 == 0 and num % 55 == 0:
+            print("Fizbuzz", end=" ")
+        elif num % 3 == 0:
+            print("Fizz", end=" ")
+        elif num % 5 == 0:
+            print("Buzz", end=" ")
+        else:
+            print(num, end=" ")
+
+fizzbuzz()
