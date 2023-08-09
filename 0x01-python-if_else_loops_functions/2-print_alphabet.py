d3d59e81e1c77a71bc6a4532651585c81d055194
mmwihaki <marymwihaki16@gmail.com>
2023-08-09 14:44:18 +0300

    pycodestyle

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for r in range(ord('a'), ord('z') + 1): #ord() get ASCII code
-    print(chr(r), end='') #chr() conver ASCII to char
+    print(f"{chr(r)}", end='') #chr() conver ASCII to char
