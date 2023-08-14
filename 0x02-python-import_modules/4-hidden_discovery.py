a520712fd98bc54efeec1f22d5e0f3baf181072f
mmwihaki <marymwihaki16@gmail.com>
2023-08-14 16:18:10 +0000

    debugging

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
+#!/usr/bin/python3
 import importlib.util
 
-module_spec = importlib.util.spec_from_file_location("hidden_4", "/Users/mwihaki/alx/alx-higher_level_programming/0x02-python-import_modules/hidden_4.pyc")
+module_spec = importlib.util.spec_from_file_location("hidden_4", "/alx-higher_level_programming/0x02-python-import_modules/hidden_4.pyc")
 module = importlib.util.module_from_spec(module_spec)
 module_spec.loader.exec_module(module)
 
