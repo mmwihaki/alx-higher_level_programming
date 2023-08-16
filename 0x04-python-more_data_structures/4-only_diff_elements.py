b8452038428fcbfe34e6e1e4588183dec6cb4755
mmwihaki <marymwihaki16@gmail.com>
2023-08-16 23:25:25 +0300

    pycodestyle

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -5,9 +5,9 @@ def only_diff_elements(set_1, set_2):
     for element in set_1:
         if element not in set_2:
             diff_set.add(element)
-    
+
     for element in set_2:
         if element not in set_1:
             diff_set.add(element)
-    
+
     return diff_set
