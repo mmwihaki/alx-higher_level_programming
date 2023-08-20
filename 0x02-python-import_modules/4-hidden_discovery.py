67340f1699eb83e3cac4d322a99b899d69138431
mmwihaki <marymwihaki16@gmail.com>
2023-08-20 22:23:19 +0300

    who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import importlib.util
+    import hidden_4
 
-module_spec = importlib.util.spec_from_file_location("hidden_4", "/alx-higher_level_programming/0x02-python-import_modules/hidden_4.pyc")
-module = importlib.util.module_from_spec(module_spec)
-module_spec.loader.exec_module(module)
-
-names = [name for name in dir(module) if not name.startswith('__')]
-names.sort()
-
+name = dir(hidden_4)
 for name in names:
-    print(name)
+    if name[:2] != "__":
+        print(name)
